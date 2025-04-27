fd441856391f725e515a862689322513f683e8a4
Harneet Dhillon <harneet.dhillon2020@gmail.com>
2025-04-27 16:49:08 -0700

    Basic REST API File Transfer w/ gRPC Setup

---
+++ rest/main.py
@@ -0,0 +1,48 @@
+import grpc
+from proto import transfer_file_pb2
+from proto import transfer_file_pb2_grpc
+
+from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+
+
+app = FastAPI()
+
+# Function for chunking file to send over gRPC
+# The full file is breaken down into chunks and each chunk is then further broken down and iterated over 
+async def generate_file_chunks(file: UploadFile, filename: str, chunk_size=1024 * 1024):
+    chunk_number = 0
+    while True:
+        chunk = await file.read(chunk_size)
+        if not chunk:
+            break
+        yield transfer_file_pb2.transferRequest(
+            data=chunk,
+            chunk_number=chunk_number,
+            filename=filename if chunk_number == 0 else ""
+        )
+        chunk_number += 1
+
+# client -> server POST Req API Endpt:
+# send .txt file 
+# make grpc call to send the .txt file to 
+@app.post("/upload-txt")
+async def upload_txt(file: UploadFile = File(...)):
+    if not file.filename.endswith(".txt"):
+        return JSONResponse(content={"error": "Only .txt files are allowed"}, status_code=400)
+    
+    # TODo: localhost needs to b changed to master IP when we have multiple nodes
+    async with grpc.aio.insecure_channel('localhost:50051') as channel:
+        stub = transfer_file_pb2_grpc.TransferFileServiceStub(channel)
+        
+        response = await stub.transferFile(generate_file_chunks(file, file.filename))
+        
+        if response.success:
+            return {"message": response.status_message}
+        else:
+            return JSONResponse(content={"error": response.status_message}, status_code=500)
+
+
+
+# Master Node -> backend post request to update new master
