be994e4df52dddc6c24112f85e1408873eb5dc2d
Harneet Dhillon <harneet.dhillon2020@gmail.com>
2025-04-27 17:55:51 -0700

    updated DIR Upload Location + Implemented REST GET Request

--- rest/main.py
+++ rest/main.py
@@ -1,7 +1,6 @@
 import grpc
-from proto import transfer_file_pb2
-from proto import transfer_file_pb2_grpc
-
+import rest_to_main_pb2
+import rest_to_main_pb2_grpc
 from fastapi import FastAPI
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
@@ -17,7 +16,7 @@ async def generate_file_chunks(file: UploadFile, filename: str, chunk_size=1024 
         chunk = await file.read(chunk_size)
         if not chunk:
             break
-        yield transfer_file_pb2.transferRequest(
+        yield rest_to_main_pb2.transferRequest(
             data=chunk,
             chunk_number=chunk_number,
             filename=filename if chunk_number == 0 else ""
@@ -26,7 +25,7 @@ async def generate_file_chunks(file: UploadFile, filename: str, chunk_size=1024 
 
 # client -> server POST Req API Endpt:
 # send .txt file 
-# make grpc call to send the .txt file to 
+# make grpc call to send the .txt file to node 
 @app.post("/upload-txt")
 async def upload_txt(file: UploadFile = File(...)):
     if not file.filename.endswith(".txt"):
@@ -34,7 +33,7 @@ async def upload_txt(file: UploadFile = File(...)):
     
     # TODo: localhost needs to b changed to master IP when we have multiple nodes
     async with grpc.aio.insecure_channel('localhost:50051') as channel:
-        stub = transfer_file_pb2_grpc.TransferFileServiceStub(channel)
+        stub = rest_to_main_pb2_grpc.TransferFileServiceStub(channel)
         
         response = await stub.transferFile(generate_file_chunks(file, file.filename))
         
@@ -42,6 +41,26 @@ async def upload_txt(file: UploadFile = File(...)):
             return {"message": response.status_message}
         else:
             return JSONResponse(content={"error": response.status_message}, status_code=500)
+        
+# client -> server GET Req API Endpt:
+# send .txt file name and file is fetched and downloaded
+@app.get("/download-txt/{filename}")
+async def download_txt(filename: str):
+    async with grpc.aio.insecure_channel('localhost:50051') as channel:
+        stub = rest_to_main_pb2_grpc.TransferFileServiceStub(channel)
+        
+        request = rest_to_main_pb2.FileRequest(filename=filename)
+        response = await stub.getFile(request)  
+        
+        file_content = BytesIO()
+        
+        async for chunk in response:
+            file_content.write(chunk.data) 
+        
+        file_content.seek(0)  
+
+        return StreamingResponse(file_content, media_type="text/plain", headers={"Content-Disposition": f"attachment; filename={filename}"})
+
 
 
 
